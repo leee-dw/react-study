7b4558752baba0695028d6e7109de467adc02ad3
Dongwoo Lee <likedemian@me.com>
2018-12-13 23:28:01 +0900

    Add: Upladed

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,73 +1,45 @@
-import React, { Component } from 'react';
-import CreateForm from './components/CreateForm'
-import TodoList from './components/TodoList'
-
-
-import './App.sass';
-
-
-const bulkTodos = (()=>{
-  const array =[]
-  for(let i = 0; i < 5000; i++) {
-    array.push({
-      id: i,
-      text: `Todo #${i}`,
-      checked: false
-    })
-  }
-  return array
-})()
-
+import React, {
+  Component
+} from 'react';
+import Form from './ components/Form'
+import './App.sass'
 
 class App extends Component {
-  id = 5000
+  id = 3
   state = {
-    todos: bulkTodos
+    todos: [
+      {
+        id: 0,
+        text: '앵귤러',
+        checked: true
+      }
+    ]
   }
 
-
-  handleCreate = text => {
+  handleCreate(text) {
     const todoData = {
       id: this.id++,
       text,
       checked: false
     }
+    
     this.setState({
       todos: this.state.todos.concat(todoData)
     })
+    console.log(this.setState)
+    
   }
 
-  handleCheck = id => {
-    this.setState(({todos})=> ({
-      todos: todos.map(
-        todo => (todo.id === id ? {...todo, checked: !todo.checked}: todo)
-      )
-    }))
-  }
 
-  handleRemove = id => {
-    this.setState(({todos})=> ({
-      todos: todos.filter(todo => todo.id !== id)
-    }))
-  }
-
-  
   render() {
-    return (
-      <div className="App">
-        <div className="header">
-          <h1>오늘 뭐할까?</h1>
-        </div>
-        <CreateForm onSubmit={this.handleCreate} />
-        <div className="white-box">
-          <TodoList
-            todos={this.state.todos}
-            onCheck={this.handleCheck} 
-            onRemove={this.handleRemove}
-          />
-        </div>
-      </div>
-    )
+
+    return ( 
+    <>
+      <Form 
+        onSumit={this.handleCreate}
+      />
+    </>
+    );
   }
 }
 
